fc197ef44cdbad8818e39a6a678ad29a1e2975d2
mango <zhangguodong0515@sina.com>
2025-01-03 14:52:47 +0800

    添加获取当前系统时间的工具文件

---
+++ utils/time_manager.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime
+
+class TimeManager:
+    _instance = None
+    _timestamp = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(TimeManager, cls).__new__(cls)
+            # 初始化时间戳，格式：2025-01-02-16_50_02
+            cls._timestamp = datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
+        return cls._instance
+
+    @classmethod
+    def get_timestamp(cls):
+        if cls._timestamp is None:
+            cls._instance = cls()
+        return cls._timestamp
